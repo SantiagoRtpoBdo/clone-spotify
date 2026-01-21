2158b22eeb12b87839f1b45f8bc94f35bdcb3792
santiagorestrepobdo <santiago.restrepo@geeks5g.com>
2026-01-21 12:42:45 -0500

    arreglos de despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel(),
+    integrations: [tailwind(), react()],
+    output: "server",
+    adapter: vercel(),
 });
